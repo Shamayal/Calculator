3f0b8a23ce83ffb4f3ed4385cbe28da011b4fca7
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-16 22:47:44 -0400

    Added if/else statements for = operator

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ const calculate = document.querySelectorAll(".operator").forEach(function (item)
     console.log(e.target.innerHTML);
   });
 });
+
+let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+  inputValue.innerText = eval(inputValue.innerText);
+} else if (e.target.innerHTML === "AC") {
+  inputValue.innerText = 0;
+} else if (e.target.innerHTML === "DE") {
+  inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
+  if (inputValue.innerText.length == 0) {
+    inputValue.innerText = 0;
+  }
+} else {
+  if (!isNaN(lastValue)) {
+    inputValue.innerText += e.target.innerHTML;
+  }
+}
