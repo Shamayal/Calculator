c6cd72a4c606d0e04eff1c667c6d4def3b3fbd42
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-16 22:19:50 -0400

    Added calculate function

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ const number = document.querySelectorAll(".number").forEach(function (item) {
     inputValue.innerText += e.target.innerHTML.trim();
   });
 });
+
+const calculate = document.querySelectorAll(".operator").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    console.log(e.target.innerHTML);
+  });
+});
