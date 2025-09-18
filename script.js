0324ee8551279d14f18fec86ebb67f839f873ff1
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-17 20:13:46 -0400

    Debugging e issue

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 // Show input/output for operations performed in the calculator
 const inputValue = document.getElementById("display");
 
-const number = document.querySelectorAll(".number").forEach(function (item) {
+document.querySelectorAll(".number").forEach(function (item) {
   item.addEventListener("click", function (e) {
-    if (inputValue.innerText === "NaN") {
-      inputValue.innerText = "";
-    }
-    if (inputValue.innerText === "0") {
+    if (inputValue.innerText === "NaN" || inputValue.innerText === "0") {
       inputValue.innerText = "";
     }
     inputValue.innerText += e.target.innerHTML.trim();
   });
 });
 
-const calculate = document.querySelectorAll(".operator").forEach(function (item) {
+document.querySelectorAll(".operator").forEach(function (item) {
   item.addEventListener("click", function (e) {
-    console.log(e.target.innerHTML);
-  });
-});
+    const value = e.target.innerHTML.trim();
+    let current = inputValue.innerText;
+    let lastChar = current.slice(-1);
+    console.log(value, current, lastChar)
+  })
+})
 
-let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
-if (!isNaN(lastValue) && e.target.innerHTML === "=") {
-  inputValue.innerText = eval(inputValue.innerText);
-} else if (e.target.innerHTML === "AC") {
-  inputValue.innerText = 0;
-} else if (e.target.innerHTML === "DE") {
-  inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
-  if (inputValue.innerText.length == 0) {
-    inputValue.innerText = 0;
-  }
-} else {
-  if (!isNaN(lastValue)) {
-    inputValue.innerText += e.target.innerHTML;
-  }
-}
+
+// let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+// if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+//   inputValue.innerText = eval(inputValue.innerText);
+// } else if (e.target.innerHTML === "AC") {
+//   inputValue.innerText = 0;
+// } else if (e.target.innerHTML === "DE") {
+//   inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
+//   if (inputValue.innerText.length == 0) {
+//     inputValue.innerText = 0;
+//   }
+// } else {
+//   if (!isNaN(lastValue)) {
+//     inputValue.innerText += e.target.innerHTML;
+//   }
+// }
